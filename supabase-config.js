d2046d09569194835cff7ded48801163b05e7e30
Lcwebdesigner <luswaclinton2@gmail.com>
2026-09-24 09:37:53 -0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = "https://noymczhhaqpjnhvvwpmf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_uk5CkQiYL6BQg0WYZD7d4g_AdTks5U8";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
